4eea7eca27b59bec8826ab455bdd5e1bcb50dd3e
Yone <yone.moreno101@alu.ulpgc.es>
2018-02-13 06:54:07 +0000

    icon

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,7 +4,9 @@ const Header = () => {
     return (
         <nav className="navbar navbar-light bg-faded">
             <a href="/" className="navbar-brand">
-                <img src="../../images/plsght.png" alt="pluralsight logo"/>
+                <img width="15px" height="15px"
+                     src="https://lh3.googleusercontent.com/9Sz9ezINa2MxhHdD_JvgSURUdsrSeslGxX-krVLWgl1foGVl2ckCmJBpoVP8lGws4-Y=w300"
+                     alt="pluralsight logo"/>
             </a>
             <ul className="navbar navbar-nav">
                 <li><a href="/">Home</a></li>
