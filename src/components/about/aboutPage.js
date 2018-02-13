0ad5b9ab8a78095b3bdd70aa54340b00d9004066
Yone <yone.moreno101@alu.ulpgc.es>
2018-02-13 10:02:42 +0000

    TODO discover how to intercept and handle transition from a page

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -1,6 +1,18 @@
 import React from 'react';
 
 const About = () => {
+    window.addEventListener('beforeunload', (event) => {
+        if (!(window.confirm("DO YOU really want to exit a fun page like this?"))) {
+            event.preventDefault();
+        }
+    });
+
+    window.onbeforeunload = (event) => {
+        if (!(window.confirm("DO YOU really want to exit a fun page like this?"))) {
+            event.preventDefault();
+        }
+    };
+
     return (
         <div>
             <h1>About</h1>
