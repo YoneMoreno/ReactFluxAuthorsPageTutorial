cf109c23c5fd1ddecd4c93db4d25414d0e1abfbf
Yone <yone.moreno101@alu.ulpgc.es>
2018-02-13 06:29:07 +0000

    routing with own functions

---
+++ src/components/about/aboutPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This app uses:</p>
+            <ul>
+                <li>React</li>
+                <li>Flux</li>
+                <li>Bootstrap</li>
+            </ul>
+        </div>
+    );
+};
+
+export {About}
--- src/index.js
+++ src/index.js
@@ -2,5 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {Home} from "./components/HomePage";
+import {About} from "./components/about/aboutPage";
 
-ReactDOM.render(<Home/>, document.getElementById('root'));
+class App extends React.Component {
+    render() {
+        var Child;
+
+        switch (this.props.route) {
+            case 'about' :
+                Child = About;
+                break;
+            default:
+                Child = Home;
+        }
+
+        return (
+            <div>
+                <Child/>
+            </div>
+        );
+    }
+}
+
+function render() {
+    var route = window.location.hash.substr(1);
+    ReactDOM.render(<App route={route}/>, document.getElementById('root'));
+}
+
+window.addEventListener('haschange', render);
+render();
+
+
