58d02efe967679141eb0548e46f554eaf89df83a
Yone <yone.moreno101@alu.ulpgc.es>
2018-02-13 06:12:26 +0000

    homepage

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+
+    return (
+        <div className="jumbotron">
+            <h1>Pluralsight administration</h1>
+            <p>React, Flux</p>
+        </div>
+    );
+};
+
+export {Home}
