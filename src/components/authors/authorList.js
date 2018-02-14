e07ed1a985aa11a97e907453fc715ec58228c880
Yone <yone.moreno101@alu.ulpgc.es>
2018-02-14 17:26:59 +0000

    deleteAuthor in authorList

--- src/components/authors/authorList.js
+++ src/components/authors/authorList.js
@@ -2,12 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types'
 import Link from "react-router-dom/es/Link";
 import {ManageAuthorPage} from "./manageAuthorPage";
+import AuthorActions from "../api/authorApi";
 
 const AuthorList = (props) => {
+
+    const deleteAuthor = (id, event) => {
+        event.preventDefault();
+        AuthorActions.deleteAuthor(id);
+        toastr.sucess('Deleted');
+    };
 
     const createAuthorRow = function (author) {
         return (
             <tr key={author.id}>
+                <td><a href="#" onClick={this.deleteAuthor.bind(this, author.id)}>Delete</a></td>
                 <td><Link to={"author/" + author.id}>{author.id}</Link></td>
                 <td>{author.firstName} {author.lastName}</td>
             </tr>
@@ -21,6 +29,7 @@ const AuthorList = (props) => {
             <table className="table">
                 <thead>
                 <tr>
+                    <th></th>
                     <th>ID</th>
                     <th>Name</th>
                 </tr>
